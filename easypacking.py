78b4795cffcd7982b6d1ae9064dba127b2d93981
Gene Wang <gene.wang@highlight.mobi>
2022-03-21 14:44:00 +0800

    commit

--- easypacking.py
+++ easypacking.py
@@ -6,7 +6,7 @@ import subprocess
 
 def cmd():
     subprocess.check_call(['git', 'pull'])
-    subprocess.check_call(['cd', '../coocoowa/'])
+    subprocess.check_call(['cd ../coocoowa/'], shell=True)
     subprocess.check_call(['git', 'restore', '.'])
     subprocess.check_call(['git', 'apply', '../MyPatch/patch.diff'])
     subprocess.check_call(['python3', 'script/build.py', '--mod', 'Gb', '-d', '1', '--path', '/home/gene/Android/Sdk/build-tools/28.0.3', '-v', '9.0.0_9000000'])
